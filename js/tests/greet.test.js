dc407efed37fce193827a89ce0d84216fbe67ed0
Candice <candice.m.sprout@gmail.com>
2018-05-18 13:56:11 +0200

    Additional test asserts

--- js/tests/greet.test.js
+++ js/tests/greet.test.js
@@ -6,4 +6,16 @@ describe('Greet Me', function(){
     it('should greet Andre in English', function(){
         assert.equal(greetings('english','Andre'), "Hello Andre");
     });
+    it('should greet Andrew in Sesotho', function(){
+        assert.equal(greetings('sesotho','Andrew'), "Dumela Andrew");
+    });
+    it('should greet Yegan in French', function(){
+        assert.equal(greetings('french','Yegan'), "Bonjour Yegan");
+    });
+    it('should count how many people have been greeted', function(){
+        assert.equal(greetings('', 2), 2);
+    });
+    it('should NOT increment counter if no name has been entered', function(){
+        assert.equal(greetings('',''), 0);
+    });
 });
