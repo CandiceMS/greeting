65a3257c55df138fbb9036b35118cb9cdcf3dd0c
Candice <candice.m.sprout@gmail.com>
2018-05-21 15:47:40 +0200

    Factory function changes

--- js/greet.js
+++ js/greet.js
@@ -1,24 +1,86 @@
 var greetCount = 0;
 var namesList = [];
 
-function greetings(checkedLanguage, greetName) {
-  var name = greetName;
+function Greetings(checkedLanguage, greetName) {
   var greet = '';
 
-      if (checkedLanguage === "english") {
-        greet = "Hello "
-      }
-      else if (checkedLanguage === "sesotho") {
-        greet = "Dumela "
-      }
-      else if (checkedLanguage === "french") {
-        greet = "Bonjour "
-      }
+  if (greetName !== "") {
+    namesList.push(greetName);
+  }
 
-      if (greetName !== "") {
-      greetCount += 1;
-      namesList.push(name);
+  // var foundTheName = false;
+  // for (var i = 0; i < namesList.length; i++) {
+  //   if (namesList[i] === greetName) {
+  //     foundTheName = true;
+  //     break;
+  //   }
+  // }
+  // if (!foundTheName){
+  //     namesList.push(greetName);
+  // }
+  console.log(namesList);
+
+  function greetPerson(checkedLanguage, greetName) {
+    if (checkedLanguage === "english") {
+      greet = "Hello "
+    } else if (checkedLanguage === "sesotho") {
+      greet = "Dumela "
+    } else if (checkedLanguage === "french") {
+      greet = "Bonjour "
     }
-    console.log(namesList);
-      return greet+name;
-    }
+    return greet + greetName;
+  }
+
+  function greetCounter() {
+    namesList.length = greetCount;
+  }
+
+  return {
+    greetPerson,
+    greetCounter
+  }
+}
+
+
+
+
+  // for (var i = 0; i < namesList.length; i++) {
+  //   if (namesList[i] !== name) {
+  //     namesList.push(name);
+  //     greetCount += 1;
+  //   } else if (namesList[i] == name) {
+  //     !namesList.push(name);
+  //   }
+  // }
+  // console.log(namesList);
+  //
+
+
+//    function counterNames() {
+// for (var i = 0; i < namesList.length; i++) {
+//   if (namesList[i] !== name) {
+//     namesList.push(name);
+//     greetCount += 1;
+//   }
+//   else if (namesList[i] == name) {
+//     !namesList.push(name);
+//     return greetCount;
+//   }
+// }
+//  }
+
+//    function counterNames() {
+// for (var i = 0; i < namesList.length; i++) {
+//   if (namesList[i] !== name) {
+//     namesList.push(name);
+//     greetCount += 1;
+//   }
+//   else if (namesList[i] == name) {
+//     !namesList.push(name);
+//   }
+// }
+//  console.log(namesList);
+//  }
+
+//return greetPerson;
+//}
--- js/greet_dom.js
+++ js/greet_dom.js
@@ -1,10 +1,15 @@
 var nameBox = document.querySelector('.nameBox');
 var greetMe = document.querySelector('.buttons');
+// var language = document.querySelector("input[name='language']:checked");
+//console.log(language);
+//  var selectedLanguage = language.value;
 var languages = document.querySelectorAll('.languages');
 var greetMeDisplay = document.querySelector('output[name="greetingResult"]');
 
 var countNames = document.querySelector('output[name="countResult"]');
 // console.log(countNames);
+
+var greetings = Greetings();
 
 function greetDom() {
   var checkedBtn = '';
@@ -12,13 +17,12 @@ function greetDom() {
   for (var i = 0; i < languages.length; i++) {
     if (languages[i].checked) {
       checkedBtn = languages[i].value;
-        languages[i].checked = false;
     }
   }
-  
-  greetMeDisplay.innerHTML = greetings(checkedBtn, nameBox.value);
+
+  greetMeDisplay.innerHTML = greetings.greetPerson(checkedBtn, nameBox.value);
   nameBox.value = "";
   countNames.innerHTML = greetCount;
 
-  }
+}
 greetMe.addEventListener('click', greetDom);
